1389a6f096654b6e8ec1b680d6735d18a9907378
Atya22 <aytaj2002eva@gmail.com>
2024-05-19 01:59:44 +0400

    added missing whitespace

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", " if i <8 else "\n")
+        print("{}{}".format(i, j), end=", " if i < 8 else "\n")
