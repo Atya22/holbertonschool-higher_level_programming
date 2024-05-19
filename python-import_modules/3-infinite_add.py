ab3719b6a2ad7d7dccdb0a27cd1e94cba96b158b
Atya22 <aytaj2002eva@gmail.com>
2024-05-19 23:28:28 +0400

    Infinite addition

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,38 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def func():
-    argc = len(argv)
-
-    if argc == 1:
-        print("0 arguments.")
-    elif argc == 2:
-        print("1 argument:\n1: {}".format(argv[1]))
-    else:
-        print("{} arguments:".format(argc - 1))
-        for i in range(1, argc):
-            print("{}: {}".format(i, argv[i]))
-
 
 if __name__ == "__main__":
-    func()
-#!/usr/bin/python3
-from sys import argv
+    """Print the addition of all arguments."""
+    import sys
 
-print(dir())
-
-def func():
-    argc = len(argv)
-    sum = 0
-
-    for i in range(1, argc):
-        sum += int(argv[i])
-
-    print(sum)
-
-
-print(dir())
-
-if __name__ != "__main__":
-    func()
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
