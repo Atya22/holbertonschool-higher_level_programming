bb77255fabdafb5782aae2b2b2ebfb2d28935e7e
Atya22 <aytaj2002eva@gmail.com>
2024-05-22 21:43:44 +0400

    changed the structure

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def func():
-    print(a)
-
-
 if __name__ == "__main__":
-    func()
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
