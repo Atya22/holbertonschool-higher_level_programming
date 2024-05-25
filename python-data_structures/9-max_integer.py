6d68b3fef6f9bc61ce9c2390e659f2e9f3579c62
Atya22 <aytaj2002eva@gmail.com>
2024-05-25 21:07:20 +0400

    case if the list is empty

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 
 def max_integer(my_list=[]):
     max = 0
+    if len(my_list) < 1:
+        return (None)
     for i in range(len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
@@ -9,6 +11,6 @@ def max_integer(my_list=[]):
 
 
 if __name__ == "__main__":
-    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+    my_list = []
     max_value = max_integer(my_list)
     print("Max: {}".format(max_value))
