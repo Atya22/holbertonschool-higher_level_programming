f43b26e94338297d927be1d81939cf61cc0c72b1
Atya22 <aytaj2002eva@gmail.com>
2024-05-25 19:31:19 +0400

    fixed error

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=' ')
-    print()
+            end_char = " " if j != len(matrix[i]) - 1 else ""
+            print("{:d}".format(matrix[i][j]), end=end_char)
+        print()
 
 
 if __name__ == "__main__":
@@ -12,7 +13,7 @@ if __name__ == "__main__":
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
-    ]
+     ]
 
 print_matrix_integer(matrix)
 print("--")
