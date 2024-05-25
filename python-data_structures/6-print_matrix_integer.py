3d295ff0a5a0db4a2f5ac1502cf9b47243295414
Atya22 <aytaj2002eva@gmail.com>
2024-05-25 19:41:29 +0400

    removed main

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,15 +6,3 @@ def print_matrix_integer(matrix=[[]]):
             end_char = " " if j != len(matrix[i]) - 1 else ""
             print("{:d}".format(matrix[i][j]), end=end_char)
         print()
-
-
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-     ]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
