1a8034f0fac02d70eef3d8f41119f7c9949fbe44
Atya22 <aytaj2002eva@gmail.com>
2024-05-25 22:24:44 +0400

    fix your main file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,8 @@
 def divisible_by_2(my_list=[]):
     list_result = []
     for i in range(len(my_list)):
-       if my_list[i] % 2 == 0:
-          list_result.append(True)
-       else:
-          list_result.append(False)
+        if my_list[i] % 2 == 0 and my_list[i] > 0:
+            list_result.append(True)
+        else:
+            list_result.append(False)
     return list_result
