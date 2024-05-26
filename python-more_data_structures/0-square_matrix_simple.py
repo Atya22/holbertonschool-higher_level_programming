ba3b0867f69617b2afe3c7ecb260311fe873f35d
Atya22 <aytaj2002eva@gmail.com>
2024-05-26 19:43:54 +0400

    fixed the loop

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+\#!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
     return [[x**2 for x in i] for i in matrix]
@@ -6,11 +6,11 @@ def square_matrix_simple(matrix=[]):
 
 if __name__ == "__main__":
     matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-        ]
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+            ]
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
