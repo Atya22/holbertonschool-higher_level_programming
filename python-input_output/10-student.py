6917cf89f03e2d3771c145b8c82054c8cb5d1065
Atya22 <aytaj2002eva@gmail.com>
2024-06-07 19:43:46 +0400

    fixed pycodestyle

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,9 +13,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        """Get a dictionary representation of the Student."""
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
