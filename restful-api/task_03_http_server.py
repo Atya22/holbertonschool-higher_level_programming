6d08d04f2a4fbb8946e624461baa27b9f121f5c3
Atya22 <aytaj2002eva@gmail.com>
2024-06-12 13:09:37 +0400

    Developing a simple API using Python with the  module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import http.server
+
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = '{"name": "John", "age": 30, "city": "New York"}'
+            self.wfile.write(data.encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
+    
+
+if __name__ == "__main__":
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, SimpleAPIHandler)
+    httpd.serve_forever()
