4928cf2130546820edf723e26c46da84a19e29d6
Atya22 <aytaj2002eva@gmail.com>
2024-06-27 19:46:23 +0400

    Update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,20 +3,8 @@
 
 
 class MyList(list):
-    """custom action"""
+    """Implements sorted printing for the built in list class."""
+
     def print_sorted(self):
-        """sorting"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
-
-
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
-
