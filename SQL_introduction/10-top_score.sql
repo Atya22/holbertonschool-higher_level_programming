f6de7a2e722c762293078a2424cfc934fcdf925d
Atya22 <aytaj2002eva@gmail.com>
2024-06-27 16:15:27 +0400

    Delete previous data

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,15 +1,3 @@
 -- Lists all records of the table second_table.
 -- Records are ordered by descending score.
-CREATE TABLE IF NOT EXISTS `second_table` (
-    `id` INT,
-    `name` VARCHAR(256),
-    `score` INT
-);
-
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
-
 SELECT score, name FROM second_table ORDER BY score DESC;
