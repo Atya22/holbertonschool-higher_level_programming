0e756dff29226934a8ac2ba079912d7b35f0dd84
Atya22 <aytaj2002eva@gmail.com>
2024-06-27 21:08:39 +0400

    Abstract Animal Class and its Subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Abstract Base Class (ABC) example. """
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
