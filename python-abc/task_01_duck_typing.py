b0bc5d9f687304a31b3062cce95785dff736ce4d
Atya22 <aytaj2002eva@gmail.com>
2024-06-27 23:35:39 +0400

    Shapes, Interfaces, and Duck Typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Defines a Rectangle subclass Square. """
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * abs(self.radius)
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    area = shape.area()
+    perimeter = shape.perimeter()
+    print(f"Area: {area}\nPerimeter: {perimeter}")
+
