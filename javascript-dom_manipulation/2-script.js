a367043d8f12b4293ed4f6e14b1095bb81f7d83d
Atya22 <aytaj2002eva@gmail.com>
2024-07-28 16:56:41 +0400

    Add  class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const redHeader = document.querySelector("#red_header");
+  redHeader.addEventListener('click', function () {
+    const header = document.querySelector('header');
+    header.classList.add('red');
+  });
+});
