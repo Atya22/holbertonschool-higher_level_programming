58172be31da686da3840b4d6fb90f11ae033d384
Atya22 <aytaj2002eva@gmail.com>
2024-07-28 17:07:46 +0400

    Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleHeader = document.querySelector('#toggle_header');
+  toggleHeader.addEventListener('click', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle('red');
+    header.classList.toggle('green');
+  });
+});
